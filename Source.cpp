5e68a671899a85f98e29b6ce98470c94332dc371
James Luke Pendleton <99436608+jp-industrial@users.noreply.github.com>
2023-02-16 18:34:41 +0000

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include "C:\Users\james\Desktop\std_lib_facilities.h"
+
+int main()
+
+{
+	cout << "please enter your name, followed by the enter key.\n";
+
+	string first_name; //declares variable type string, assigns name.
+
+	cin >> first_name; //accept input, feed into variable. Provides a delay for input!!!!
+
+	cout << "hello, " << first_name << "!\n"; //concatenated text output.
+	cout << "first_name" << " is " << first_name; 
+}
